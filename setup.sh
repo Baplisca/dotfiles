6b8c026ee504669938733076ad58cff244fad3c3
Baplisca <yokota0327@gmail.com>
2023-10-03 22:18:02 +0900

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
-set -e
+set -euo
 echo "Build a development environment"
 
-DOTFILES_REPO="$HOME/dotfiles"
+DOTFILES_REPO=~/dotfiles
 # 設定ファイルごとにシンボリックリンクを作成する
-cd "$DOTFILES_REPO"
+cd ${DOTFILES_REPO}
+
+# Gitリポジトリ内に存在するファイルの一覧で.から始まるファイルを抽出
 git ls-files | grep -e '^\.' | while read DOTFILE; do
+  # _があるファイルをスキップする(.gitconfig_company, .gitconfig_private, .gitignore_global)
   if [[ "$DOTFILE" =~ "_" ]]; then
     echo "skip the symbolic link creation for: $DOTFILE"
   else
     echo "create a symbolic link: $DOTFILE"
-    ln -sf "$DOTFILES_REPO/$DOTFILE" "$HOME/$DOTFILE"
-    # -sオプションは、シンボリックリンクの作成を表す
-    # -fオプションは、作成先にファイルがあるとき、それを削除してから実行することを表す
+    # -s シンボリックリンクの作成を表す
+    # -f 作成先にファイルがあるとき、それを削除してから実行することを表す
+    ln -sf ${DOTFILES_REPO}/${DOTFILE} ~/${DOTFILE}
   fi
 done
 
-BREWFILE="$HOME/.Brewfile"
-if [ -e "$BREWFILE" ]; then
+BREWFILE=~/.Brewfile
+if [ -e "${BREWFILE}" ]; then
   echo "Install Homebrew packages all at once"
-  brew bundle  --file "$BREWFILE"
+  brew bundle --file "$BREWFILE"
 fi
 
 echo "Development environment setup is complete!"
